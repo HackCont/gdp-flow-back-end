e03b677086955955add76503bac598361297795b
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 17:11:23 -0300

    feat(User): Create user entity

---
+++ Models/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace GdpFlow.API.Models.Entities;
+
+public class User
+{
+	public Guid Id { get; set; }
+	public required string Email { get; set; }
+	public required string FirstName { get; set; }
+	public required string LastName { get; set; }
+	public string? Phone { get; set; }
+	public string? Bio { get; set; }
+	public DateTime CreatedAt { get; set; }
+}
