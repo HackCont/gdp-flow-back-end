c8e99fe650f21c92d9c6bf22c0f64a5044de27f9
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 17:17:21 -0300

    feat(database): Configure migration for database
    - Execute database migration on app initialization

---
+++ Extensions/MigrationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using GdpFlow.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GdpFlow.API.Extensions;
+
+public static class MigrationExtensions
+{
+	public static IHost UseMigrateDatabase(this IHost host)
+	{
+		using (var scope = host.Services.CreateScope())
+		{
+			var services = scope.ServiceProvider;
+
+			try
+			{
+				var context = services.GetRequiredService<AppDbContext>();
+
+				context.Database.Migrate();
+			}
+			catch (Exception ex)
+			{
+
+				throw new InvalidOperationException("An error occurred while migrating the database.", ex);
+			}
+		}
+		return host;
+	}
+}
