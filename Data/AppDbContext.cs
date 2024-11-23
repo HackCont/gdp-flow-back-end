45ff3b27452c308d2cffcbf837b3973504cb9158
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 17:19:46 -0300

    feat(database): Create context for user table

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GdpFlow.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GdpFlow.API.Data;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+	}
+
+	public required DbSet<User> Users { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.HasPostgresExtension("uuid-ossp")
+			.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+	}
+}
