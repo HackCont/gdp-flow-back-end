d539f3e290a757e201e9c2df9707601496261d61
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 17:10:49 -0300

    feat(Keycloak): Add keycloak settings model

---
+++ Models/Settings/KeycloakSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace GdpFlow.API.Models.Settings;
+
+public class KeycloakSettings
+{
+	public required string Authority { get; set; }
+	public required string Audience { get; set; }
+	public required bool RequireHttpsMetadata { get; set; }
+	public required string ClientId { get; set; }
+	public required string ClientSecret { get; set; }
+	public required KeycloakUri Uri { get; set; }
+}
+
+public class KeycloakUri
+{
+	public required string Register { get; set; }
+	public required string Login { get; set; }
+}
