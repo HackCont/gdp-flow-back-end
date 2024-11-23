85d08406012153408665a1e6f86e2b4eeeacc514
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 19:49:23 -0300

    feat(register): Create user register controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using GdpFlow.API.Models.DTOs.User.Register;
+using GdpFlow.API.Services.UserServices.Register;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GdpFlow.API.Controllers;
+[Route("api/users/")]
+[ApiController]
+[Produces("application/json")]
+public class AuthController : ResultsControllerBase
+{
+	private readonly IRegisterUserService _registerUserService;
+
+	public AuthController(IRegisterUserService registerUserService)
+	{
+		_registerUserService = registerUserService;
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> Register([FromBody] RegisterDTO registerDTO)
+	{
+		var result = await _registerUserService.RegisterAsync(registerDTO);
+		return CustomResult(result);
+	}
+}
