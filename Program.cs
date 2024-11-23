f773753aaa6ed6ed90884c8667f818a4a9b86787
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 17:09:26 -0300

    chore: Configure middleware and services

--- Program.cs
+++ Program.cs
@@ -1,21 +1,50 @@
+using FluentValidation;
+using GdpFlow.API.Data;
+using GdpFlow.API.Extensions;
+using GdpFlow.API.Models.DTOs.User.Register;
+using GdpFlow.API.Models.Settings;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerSetup();
+
+builder.Services.AddAuthenticationSetup(builder.Configuration);
+
+builder.Services.AddValidatorsFromAssemblyContaining<RegisterValidator>();
+
+builder.Services.AddHttpClient();
+
+builder.Services.Configure<KeycloakSettings>(builder.Configuration.GetSection("Keycloak"));
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+	options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnection")));
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+app.UseGlobalExceptionHandler();
+
+//if (app.Environment.IsDevelopment())
+//{
 	app.UseSwagger();
 	app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+if (app.Environment.IsProduction())
+{
+	app.UseMigrateDatabase();
+}
 
 app.Run();
