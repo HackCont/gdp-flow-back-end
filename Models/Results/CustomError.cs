2ffe0c335df1e4567b8bf20a32fa89b1210b918c
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 19:48:23 -0300

    feat: Create custom results

---
+++ Models/Results/CustomError.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+
+namespace GdpFlow.API.Models.Results;
+
+public class CustomError : Error
+{
+	public CustomError(string message, int statusCode) : base(message) { StatusCode = statusCode; }
+	public int StatusCode { get; set; }
+}
---
+++ Models/Results/CustomSuccess.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+
+namespace GdpFlow.API.Models.Results;
+
+public class CustomSuccess : Success
+{
+	public CustomSuccess(string message, int statusCode) : base(message) { StatusCode = statusCode; }
+	public int StatusCode { get; set; }
+}
