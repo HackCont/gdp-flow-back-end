83b91dbf2eb6a779a8a5d21311492fff383f991f
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 17:18:24 -0300

    feat(error): Configure global error middleware on app

---
+++ Extensions/ExceptionHandlingExtension.cs
@@ -0,0 +1,8 @@
+﻿using GdpFlow.API.Middlewares;
+
+namespace GdpFlow.API.Extensions;
+
+public static class ExceptionHandlingExtension
+{
+	public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder builder) => builder.UseMiddleware<ExceptionHandlingMiddleware>();
+}
