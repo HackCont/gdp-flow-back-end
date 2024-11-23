c4cb440dcfa53d37e47c57c50cfe053c7e51aabf
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 20:24:01 -0300

    feat(CORS): Configure Cors policy

---
+++ Extensions/CorsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using GdpFlow.API.Models.Settings;
+
+namespace GdpFlow.API.Extensions;
+
+public static class CorsExtensions
+{
+	public static IServiceCollection AddCorsSetup(this IServiceCollection services, IConfiguration configuration)
+	{
+		var corsSettings = configuration.GetSection("Cors").Get<CorsSettings>() ??
+			throw new ArgumentNullException(nameof(configuration), "Cors settings not found in configurations");
+
+		services.AddCors(options =>
+		{
+			options.AddPolicy("default", policy =>
+			{
+				//policy
+				//	.WithOrigins(corsSettings?.AllowedOrigins ?? Array.Empty<string>())
+				//	.AllowAnyMethod()
+				//	.AllowAnyHeader()
+				//	.AllowCredentials();
+				policy
+					.SetIsOriginAllowed(_ => true)
+					.AllowAnyMethod()
+					.AllowAnyHeader()
+					.AllowCredentials();
+			});
+		});
+		return services;
+	}
+}
