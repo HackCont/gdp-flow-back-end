8e7c8754ae3a1f0b17ae3f800666817f82e5687a
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 17:12:21 -0300

    feat(keycloak): Create DTO for admin access token

---
+++ Models/DTOs/User/Register/AdminTokenResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GdpFlow.API.Models.DTOs.User.Register;
+
+public class AdminTokenResponseDTO
+{
+	[JsonPropertyName("access_token")]
+	public string? AccessToken { get; set; }
+}
