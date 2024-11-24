787048768210d6fca2bb5b8207934514bb277703
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 23:17:34 -0300

    feat(login): Create dto for login request

---
+++ Models/DTOs/User/Login/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GdpFlow.API.Models.DTOs.User.Login;
+
+public class LoginDTO
+{
+	[Required(ErrorMessage = "Email is required")]
+	[EmailAddress(ErrorMessage = "Invalid email address")]
+	public required string Email { get; set; }
+
+	[Required(ErrorMessage = "Password is required")]
+	[StringLength(12, MinimumLength = 5, ErrorMessage = "Password must be at least 5 characters long")]
+	public required string Password { get; set; }
+}
