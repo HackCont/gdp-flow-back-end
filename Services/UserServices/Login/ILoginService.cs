53babcc7aef2f641a437a0e2c6ec14eeb1b2bcd0
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 23:15:25 -0300

    feat(login): Create login service

---
+++ Services/UserServices/Login/ILoginService.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+using GdpFlow.API.Models.DTOs.User.Login;
+
+namespace GdpFlow.API.Services.UserServices.Login;
+
+public interface ILoginService
+{
+	Task<Result<LoginTokenResponseDTO>> LoginAsync(LoginDTO loginDTO);
+}
---
+++ Services/UserServices/Login/LoginService.cs
@@ -0,0 +1,60 @@
+﻿using FluentResults;
+using GdpFlow.API.Models.DTOs.User.Login;
+using GdpFlow.API.Models.Results;
+using GdpFlow.API.Models.Settings;
+using Microsoft.Extensions.Options;
+
+namespace GdpFlow.API.Services.UserServices.Login;
+
+public class LoginService : ILoginService
+{
+	public readonly IHttpClientFactory _httpClientFactory;
+	private readonly KeycloakSettings _keycloakSettings;
+	public LoginService(IHttpClientFactory httpClientFactory, IOptions<KeycloakSettings> keycloakSettings)
+	{
+		_httpClientFactory = httpClientFactory;
+		_keycloakSettings = keycloakSettings.Value;
+	}
+
+	public async Task<Result<LoginTokenResponseDTO>> LoginAsync(LoginDTO loginDTO)
+	{
+		var client = _httpClientFactory.CreateClient();
+
+		var content = new FormUrlEncodedContent(
+		[
+			new KeyValuePair<string, string>("grant_type", "password"),
+			new KeyValuePair<string, string>("client_id", _keycloakSettings.ClientId),
+			new KeyValuePair<string, string>("client_secret", _keycloakSettings.ClientSecret),
+			new KeyValuePair<string, string>("username", loginDTO.Email),
+			new KeyValuePair<string, string>("password", loginDTO.Password),
+			new KeyValuePair<string, string>("scope", _keycloakSettings.Scope),
+		]);
+
+		var loginResponse = await client.PostAsync(_keycloakSettings.Uri.Login, content);
+		if (!loginResponse.IsSuccessStatusCode)
+		{
+			return Result.Fail<LoginTokenResponseDTO>(new CustomError("Unauthorized", StatusCodes.Status401Unauthorized));
+		}
+
+		var loginResult = await loginResponse.Content.ReadFromJsonAsync<LoginTokenResponseDTO>();
+		if (loginResult == null)
+		{
+			return Result.Fail<LoginTokenResponseDTO>(new CustomError("Internal server error from login endpoint", StatusCodes.Status500InternalServerError));
+		}
+
+		var tokenResponse = new LoginTokenResponseDTO
+		{
+			AccessToken = loginResult.AccessToken,
+			ExpiresIn = loginResult.ExpiresIn,
+			RefreshExpiresIn = loginResult.RefreshExpiresIn,
+			RefreshToken = loginResult.RefreshToken,
+			TokenType = loginResult.TokenType,
+			IdToken = loginResult.IdToken,
+			NotBeforePolicy = loginResult.NotBeforePolicy,
+			SessionState = loginResult.SessionState,
+			Scope = loginResult.Scope,
+		};
+
+		return Result.Ok(tokenResponse);
+	}
+}
