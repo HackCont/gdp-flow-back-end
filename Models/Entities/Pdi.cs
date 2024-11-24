3523931b374fbbd0e69b94d04f56fb7e0e6e657f
Paulo Ricardo M <pr741234@gmail.com>
2024-11-24 07:02:07 -0300

    feat(pdi): Create pdi entity

---
+++ Models/Entities/Pdi.cs
@@ -0,0 +1,16 @@
+﻿namespace GdpFlow.API.Models.Entities;
+
+public class Pdi
+{
+	public Guid Id { get; set; }
+	public string? StartDoing { get; set; }
+	public string? StopDoing { get; set; }
+	public string? DoLess { get; set; }
+	public string? KeepDoing { get; set; }
+	public string? DoMore { get; set; }
+	public string? Goal { get; set; }
+	public DateTime CreatedAt { get; set; }
+
+	public Guid UserId { get; set; }
+	public User? User { get; set; }
+}
