89f0532e17ec1413cbd6f6bdf968fdfd5dab8151
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 23:16:06 -0300

    feat(user): Create method to get by email

---
+++ Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using GdpFlow.API.Data;
+using GdpFlow.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GdpFlow.API.Repositories.UserRepository;
+
+public class UserRepository : Repository<User>, IUserRepository
+{
+	private readonly AppDbContext _context;
+
+	public UserRepository(AppDbContext context) : base(context)
+	{
+		_context = context;
+	}
+
+	public async Task<User?> GetByEmailAsync(string email)
+	{
+		return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+	}
+}
