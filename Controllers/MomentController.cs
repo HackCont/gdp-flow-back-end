35260611d7b0ad4987f4e180c08111206fbd4b64
Paulo Ricardo M <pr741234@gmail.com>
2024-11-24 10:38:17 -0300

    feat(moment): Add create action

---
+++ Controllers/MomentController.cs
@@ -0,0 +1,25 @@
+﻿using GdpFlow.API.Models.DTOs.Moment;
+using GdpFlow.API.Services.Moments;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GdpFlow.API.Controllers;
+[Route("api/moment")]
+[ApiController]
+public class MomentController : ResultsControllerBase
+{
+	private readonly IMomentService _momentService;
+
+	public MomentController(IMomentService momentService)
+	{
+		_momentService = momentService;
+	}
+
+	[HttpPost]
+	[Authorize]
+	public async Task<IActionResult> Create([FromBody] CreateMomentDTO momentDTO)
+	{
+		var result = await _momentService.CreateAsync(momentDTO);
+		return CustomResult(result);
+	}
+}
