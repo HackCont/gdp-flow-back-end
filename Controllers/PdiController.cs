6fe8b754f477f2f23e18690606c1cac16b34d8ea
Paulo Ricardo M <pr741234@gmail.com>
2024-11-24 07:07:16 -0300

    feat(pdi): Create Update and create controller

---
+++ Controllers/PdiController.cs
@@ -0,0 +1,25 @@
+﻿using GdpFlow.API.Models.DTOs.Pdi;
+using GdpFlow.API.Services.Pdis;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GdpFlow.API.Controllers;
+[Route("api/pdi")]
+[ApiController]
+public class PdiController : ResultsControllerBase
+{
+	private readonly IPdiService _pdiService;
+
+	public PdiController(IPdiService pdiService)
+	{
+		_pdiService = pdiService;
+	}
+
+	[HttpPut]
+	[Authorize]
+	public async Task<IActionResult> UpdateOrCreate([FromBody] CreatePdiDTO createPdiDTO)
+	{
+		var result = await _pdiService.UpdateOrCreateAsync(createPdiDTO, Request);
+		return CustomResult(result);
+	}
+}
