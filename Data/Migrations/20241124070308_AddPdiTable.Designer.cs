648019b705115c2110b0049008f0d8d63a6a7371
Paulo Ricardo M <pr741234@gmail.com>
2024-11-24 07:04:31 -0300

    feat(migrate): Create Pdi migration

--- Data/Migrations/20241124070308_AddPdiTable.Designer.cs
+++ Data/Migrations/20241124070308_AddPdiTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GdpFlow.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GdpFlow.API.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241124070308_AddPdiTable")]
+    partial class AddPdiTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -73,7 +76,7 @@ namespace GdpFlow.API.Data.Migrations
                     b.HasIndex("UserId")
                         .IsUnique();
 
-                    b.ToTable("Pdis");
+                    b.ToTable("Pdi");
                 });
 
             modelBuilder.Entity("GdpFlow.API.Models.Entities.User", b =>
---
+++ Data/Migrations/20241124070308_AddPdiTable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GdpFlow.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPdiTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pdi",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    Start_Doing = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Stop_Doing = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Do_Less = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Keep_Doing = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Do_More = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Goal = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()"),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pdi", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pdi_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pdi_UserId",
+                table: "Pdi",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pdi");
+        }
+    }
+}
