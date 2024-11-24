905cedcf52ac28851ae53610680b8d8c09ecb452
Paulo Ricardo M <pr741234@gmail.com>
2024-11-24 10:37:52 -0300

    feat(moment): Configure entity

---
+++ Data/DbConfigurations/MomentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using GdpFlow.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GdpFlow.API.Data.DbConfigurations;
+
+public class MomentConfiguration : IEntityTypeConfiguration<Moment>
+{
+	public void Configure(EntityTypeBuilder<Moment> builder)
+	{
+		builder.ToTable("Moments");
+		builder.HasKey(m => m.Id);
+		builder.Property(m => m.Id)
+			.HasDefaultValueSql("uuid_generate_v4()");
+
+		builder.Property(m => m.Title)
+			.HasMaxLength(20)
+			.IsRequired();
+
+		builder.Property(m => m.Description)
+			.HasMaxLength(50)
+			.IsRequired();
+	}
+}
