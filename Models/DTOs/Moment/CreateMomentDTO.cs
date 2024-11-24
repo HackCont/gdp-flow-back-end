5b17ee1c3b6232ad19bf9d20936491f5bf67d5eb
Paulo Ricardo M <pr741234@gmail.com>
2024-11-24 10:35:55 -0300

    feat(moment): Create moment request dto

---
+++ Models/DTOs/Moment/CreateMomentDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GdpFlow.API.Models.DTOs.Moment;
+
+public class CreateMomentDTO
+{
+	public DateTime Data { get; set; }
+
+	[Required(ErrorMessage = "Title is required")]
+	[StringLength(20, ErrorMessage = "Title can't be longer than 20 characters")]
+	public required string Title { get; set; }
+
+	[Required(ErrorMessage = "Description is required")]
+	[StringLength(50, ErrorMessage = "Description can't be longer than 50 characters")]
+	public required string Description { get; set; }
+
+	public Guid UserId { get; set; }
+}
