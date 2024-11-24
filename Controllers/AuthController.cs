35f700981ecc30c251bdfeb17beb3b494f1f9084
Paulo Ricardo M <pr741234@gmail.com>
2024-11-23 23:18:16 -0300

    feat: Add login controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using GdpFlow.API.Models.DTOs.User.Register;
+﻿using GdpFlow.API.Models.DTOs.User.Login;
+using GdpFlow.API.Models.DTOs.User.Register;
+using GdpFlow.API.Services.UserServices.Login;
 using GdpFlow.API.Services.UserServices.Register;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,16 +11,27 @@ namespace GdpFlow.API.Controllers;
 public class AuthController : ResultsControllerBase
 {
 	private readonly IRegisterUserService _registerUserService;
+	private readonly ILoginService _loginService;
 
-	public AuthController(IRegisterUserService registerUserService)
+	public AuthController(IRegisterUserService registerUserService, ILoginService loginService)
 	{
 		_registerUserService = registerUserService;
+		_loginService = loginService;
 	}
 
 	[HttpPost]
+	[Route("register")]
 	public async Task<IActionResult> Register([FromBody] RegisterDTO registerDTO)
 	{
 		var result = await _registerUserService.RegisterAsync(registerDTO);
+		return CustomResult(result);
+	}
+
+	[HttpPost]
+	[Route("login")]
+	public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
+	{
+		var result = await _loginService.LoginAsync(loginDTO);
 		return CustomResult(result);
 	}
 }
