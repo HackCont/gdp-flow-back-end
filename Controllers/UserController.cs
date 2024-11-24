8a91afa407fe01c54d213a83eb5bcd2f92ad7044
Paulo Ricardo M <pr741234@gmail.com>
2024-11-24 01:09:19 -0300

    feat(user): Create controller and update user action

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using GdpFlow.API.Models.DTOs.User;
+using GdpFlow.API.Services.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GdpFlow.API.Controllers;
+[Route("api/users")]
+[ApiController]
+public class UserController : ResultsControllerBase
+{
+	private readonly IUserService _userService;
+
+	public UserController(IUserService userService)
+	{
+		_userService = userService;
+	}
+
+	[HttpPut("{userId}")]
+	public async Task<IActionResult> Update(Guid userId, UpdateDTO updateDTO)
+	{
+		var result = await _userService.UpdateUserAsync(userId, updateDTO);
+		return CustomResult(result);
+	}
+}
